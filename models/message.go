71196f662cd5c0768db6d9d14ba735103b9e6312
好饭不怕晚 <lepengxi@163.com>
2022-11-24 22:12:40 +0800

    socket + redis

--- models/message.go
+++ models/message.go
@@ -2,23 +2,19 @@ package models
 
 import "gorm.io/gorm"
 
-type UserMessage struct {
+type Message struct {
 	gorm.Model
-	Identity      string
-	Name          string
-	PassWord      string
-	Phone         string `valid:"matches(^1[3-9]{1}\\d{9}$)"`
-	Email         string
-	ClientIp      string
-	ClientPort    string
-	LoginTime     uint64
-	HeartBeatTime uint64
-	LoginOutTime  uint64
-	IsLoginOut    bool
-	DeviceInfo    string
-	Salt          string
+	FromId   string // 发送者
+	TargetId string // 接收者
+	Type     string // 消息类型：群聊、私聊
+	Media    int    //消息类型：文字、图片、音频
+	Content  string // 消息体
+	Pic      string
+	Url      string
+	Desc     string
+	Amount   int // 其他数字统计
 }
 
-func (table *UserMessage) TableName() string {
-	return "user_message" // 数据库里表的命名
+func (table *Message) TableName() string {
+	return "message" // 数据库里表的命名
 }
